05da412b4c2216bdc07b2e2bf1e447fd9515afee
zangyuechao <markcoder@outlook.com>
2019-08-10 21:17:01 +0800

    edit

--- app/api/v1/token.js
+++ app/api/v1/token.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router')
 const { TokenValidator } = require('./../../validator/validator')
 const { LoginType } = require('./../../lib/enum')
+const User = require('./../../models/user')
+const { ParameterException } = require('./../../../core/http-exception')
 const router = new Router({
     prefix: '/v1/token'
 })
@@ -9,16 +11,19 @@ router.post('/', async (ctx, next) => {
   const v = await new TokenValidator().validate(ctx)
   switch (v.get('body.type')) {
     case LoginType.USER_EMAIL:
+      await emailLogin(v.get('body.account'), v.get('body.secret'))
       break
     case LoginType.USER_MINI_PROGRAM:
-        break
+      break
     case LoginType.USER_MOBILE:
-        break
+      break
+    default:
+      throw new Error('没有处理逻辑')
   }
 })
 
 async function emailLogin (account, secret) {
-
+  const user = await User.verifyEmailPassword(account, secret)
 }
 
 module.exports = router
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,23 @@
 const bcrypt = require('bcryptjs')
 const { sequelize } = require('./../../core/db')
 const {Sequelize, Model} = require('sequelize')
+const { NotFound, AuthFailed } = require('./../../core/http-exception')
 
 class User extends Model {
  static async verifyEmailPassword (email, plainPassword) {
-  const user = User.findOne({})
+  const user = await User.findOne({
+    where: {
+      email
+    }
+  })
+  if (!user) {
+    throw new NotFound('用户不存在')
+  }
+  const correct = bcrypt.compareSync(plainPassword, user.password)
+  if (!correct) {
+    throw new AuthFailed('密码错误')
+  }
+  return user
  }
 }
 
